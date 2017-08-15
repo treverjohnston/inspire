c7747026c336278288e4d9353e4ffb387a10e5cf
Trever Johnston <treverj1029@gmail.com>
2017-08-15 15:55:40 -0600

    added 24hr clock functionality

--- public/app/components/clock/clock-controller.js
+++ public/app/components/clock/clock-controller.js
@@ -6,14 +6,49 @@ function ClockController() {
         return i;
     }
 
-   this.startTime = function startTime(){
+    function toggleTime(type){
+        if (type=='hr'){
+            startTimeHr()
+        }
+         else if(type=='military'){
+        startTime()
+        } 
+    }
+
+    function startTime() {
         var today = new Date();
         var h = today.getHours();
         var m = today.getMinutes();
         h = checkTime(h);
         m = checkTime(m);
-        document.getElementById('clock').innerHTML = `<h2 class="clock-face"><b>${h}:${m}</b></h2>`
-        var t = setTimeout(startTime, 500);
+        document.getElementById('clock').innerHTML = `<button type="button"class="btn clock-btn" onclick="app.controllers.clockController.getTime('hr')"><h2 class="clock-face"><b>${h}:${m}</b></h2></button>`
+        var t = setTimeout(toggleTime('military'), 500);
+    }
+
+    function startTimeHr() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var ampm = ''
+        if (m < 10){
+            m = '0'+m;
+        }
+        if (h == 24) {
+            h == 0
+            ampm = 'am'
+        } else if (h >= 12) {
+            ampm = 'pm'
+        } else {
+            ampm = 'am'
+        }
+        h = h % 12;
+        var time = h + ':' + m + ' ' + ampm;
+        document.getElementById('clock').innerHTML = `<button type="button"class="btn clock-btn" onclick="app.controllers.clockController.getTime('military')"><h2 class="clock-face-hr">${time}</h2></button>`
+        var t = setTimeout(toggleTime('hr'), 500);
+    }
+
+    this.getTime = function getTime(type) {
+        toggleTime(type)
     }
 
 }
